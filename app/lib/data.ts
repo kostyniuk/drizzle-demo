106d3ee7ae0a4547837bb56c005417016983c1db
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-09-30 23:08:16 +0200

    used and tested transactions in drizzle. They work :)

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,8 +1,9 @@
 'use server'
 
-import { instruments } from "@/utils/drizzle/schema"
+import { Instrument, instruments } from "@/utils/drizzle/schema"
 import db from "@/utils/drizzle/db"
 import { revalidatePath } from 'next/cache'
+import { sql } from "drizzle-orm"
 
 // List all instruments
 export const listInstruments = async () => {
@@ -10,8 +11,25 @@ export const listInstruments = async () => {
   return allInstruments;
 }
 
-export const createInstrument = async (instrument: { name: string }) => {
-  const newInstrument = await db.insert(instruments).values(instrument)
+export const createInstrument = async (instrument: { name: string }) : Promise<Instrument> => {
+  const newInstruments = await db.insert(instruments).values(instrument).returning();
+  console.log(newInstruments);
   revalidatePath('/instruments')
-  return newInstrument
+  return newInstruments[0];
 }
+
+export const createInstrumentTx = async (instrument: { name: string }) : Promise<Instrument> => {
+    let newInstrument: Instrument | undefined;
+    try {
+        await db.transaction(async (tx) => {
+            const nonExistingTable = 'error_table';
+            const result = await tx.insert(instruments).values(instrument).returning();
+            newInstrument = result[0] as Instrument;
+            await db.execute(sql`SELECT 1 from ${nonExistingTable};`);
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    revalidatePath('/instruments')
+    return newInstrument as Instrument;
+  }
--- utils/drizzle/schema.ts
+++ utils/drizzle/schema.ts
@@ -4,3 +4,9 @@ export const instruments = pgTable('instruments', {
   id: serial('id').primaryKey(),
   name: text('name'),
 });
+
+type SelectInstrument = typeof instruments.$inferSelect;
+type InsertInstrument = typeof instruments.$inferInsert;
+
+export type Instrument = SelectInstrument;
+export type NewInstrument = InsertInstrument;
